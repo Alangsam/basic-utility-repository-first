769561ca291d0ab54ffadd2c6c65cef32aaa4b02
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-05-12 20:18:02 -0700

    added all the basic functions

--- functions.js
+++ functions.js
@@ -5,123 +5,132 @@ function add(input1, input2) {
    return input1 + input2;
 }
 
-// //subract
-// function subract(input1, input2) {
-//    var total = input1 + input2;
-//    return total;
-// }
+ //subract
+ function subract(input1, input2) {
+    var total = input1 - input2;
+    return total;
+ }
 
 
-// //multiply
-// function multiply(input1, input2) {
-//    var total = input1 * input2;
-//    return total;
-// }
+//multiply
+function multiply(input1, input2) {
+   var total = input1 * input2;
+   return total;
+}
 
 // //divide
-// function divide(input1, input2) {
-//    var total = input1 / input2;
-//    return total;
-// }
+function divide(input1, input2) {
+   var total = input1 / input2;
+   return total;
+}
 
 // //increment
-// function increment(input) {
-//    var increased = input++;
-//    return increased;
-// }
+function increment(input) {
+   var increased = ++input;
+   return increased;
+}
 
 // //decrement
-// function decrement(input) {
-//    var decreased = input--;
-//    return decreased;
-// }
+function decrement(input) {
+   var decreased = --input;
+   return decreased;
+}
 
 // //multiply decimals
-// function multDec(input1, input2) {
-//    var decResult = input1 * input2;
-//    return decResult;
-// }
+function multDec(input1, input2) {
+   var decResult = input1 * input2;
+   return decResult;
+}
 
 // //divide decimals
-// function divDec(input1, input2) {
-//    var divResult = input1 / input2;
-//    return divResult;
-// }
+function divDec(input1, input2) {
+   var divResult = input1 / input2;
+   return divResult;
+}
 
 // //Concatenating Strings with Plus Operator
-// function stringCon(string1, string2) {
-//    var bothstrings = string1 + string2;
-//    return bothstrings;
-// }
+function stringCon(string1, string2) {
+   var bothstrings = string1 + " " + string2;
+   return bothstrings;
+}
 
 // //Concatenating Strings with the Plus Equals Operator
-// function stringConeq(string1, string2) {
-//    string1 += string2;
-//    return string1;
-// }
+function stringConEq(string1, string2) {
+   string1 += " " + string2;
+   return string1;
+}
 
 // //Constructing Strings with Variables
-// function mySentence(name) {
-//    var sentence = "Hi I'm " + name;
-//    return sentence;
-// }
+function mySentence(name) {
+   var sentence = "Hey you're " + name;
+   return sentence;
+}
 
 // //Find the Length of a String
-// function findLength(string) {
-//    var lengthIs = string.length;
-//    return lengthIs;
-// }
+function findLength(string) {
+   var lengthIs = string.length;
+   return lengthIs;
+}
 
 // //Use Bracket Notation to Find the First Character in a String
-// function firstGuy(string) {
-//    var firstLetter = string[0];
-//    return firstLetter;
-// }
+function firstGuy(string) {
+   var firstLetter = string[0];
+   return firstLetter;
+}
 
 // //Use Bracket Notation to Find the Nth Character in a String
-// function nthGuy(string, n) {
-//    var anyThingGoes = string[n];
-//    return anyThingGoes;
-// }
+function nthGuy(string, n) {
+   var anyThingGoes = string[n-1];
+   return anyThingGoes;
+}
 
 // //Use Bracket Notation to Find the Last Character in a String
-// function lastGuy(string) {
-//    var onlyTheLast = string[string.length];
-//    return onlyTheLast;
-// }
+function lastGuy(string) {
+   var num = string.length-1;
+   var newString = string[num];
+   return newString;
+}
 
 // //Use Bracket Notation to Find the Nth-to-Last Character in a String
-// function nthToLast(string, n) {
-//    var whateverWorks = string[string.length - n];
-//    return whateverWorks;
-// }
+function nthToLast(string, n) {
+   var whateverWorks = string[string.length - n];
+   return whateverWorks;
+}
 
 // //Manipulate Arrays With push()
-// function pushIt(array, string) {
-//    var newArray = array.push(string);
-//    return newArray;
-// }
+function pushIt(array, string) {
+   array.push(string);
+   return array;
+}
 
 // //Manipulate Arrays With pop()
-// function popIt(array) {
-//    var newwerArray = array.pop();
-//    return newwerArray;
-// }
+function popIt(array) {
+   array.pop();
+   return array;
+}
 
 // //Manipulate Arrays With shift()
-// function shiftIt(array, string) {
-//    var shiftArray = array.shift(string);
-//    return shiftArray;
-// }
+function shiftIt(array) {
+   var hmmmmm = array.shift();
+   return hmmmmm;
+}
 
 // //Manipulate Arrays With unshift()
-// function unShiftIt(array) {
-//    var unShiftArray = array.unshift();
-//    return unShiftArray;
-// }
+function unShiftIt(array, string) {
+   array.unshift(string);
+   return array;
+}
 
 // //Shopping List
-// function shopping(input1, input2, input3)
-//    var theList
+function shopping(input1, input2, input3, input4) {
+   var theShopList = "I need to get " + input1 + ", " + input2 + ", " + input3 + ", and " + input4 + ".";
+   return theShopList;
+}
 
 // //Stand in Line
+function movinOverOne(arr, item) {
+   arr.push(item);
+   arr.shift();
+ 
+   return arr;
+}   
