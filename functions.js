bc7b62fbd0f6ab7bc8fa9cb11ec77dde31e9ba18
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-05-12 18:02:37 -0700

    adding all functions

--- functions.js
+++ functions.js
@@ -4,3 +4,124 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
+
+// //subract
+// function subract(input1, input2) {
+//    var total = input1 + input2;
+//    return total;
+// }
+
+
+// //multiply
+// function multiply(input1, input2) {
+//    var total = input1 * input2;
+//    return total;
+// }
+
+// //divide
+// function divide(input1, input2) {
+//    var total = input1 / input2;
+//    return total;
+// }
+
+// //increment
+// function increment(input) {
+//    var increased = input++;
+//    return increased;
+// }
+
+// //decrement
+// function decrement(input) {
+//    var decreased = input--;
+//    return decreased;
+// }
+
+// //multiply decimals
+// function multDec(input1, input2) {
+//    var decResult = input1 * input2;
+//    return decResult;
+// }
+
+// //divide decimals
+// function divDec(input1, input2) {
+//    var divResult = input1 / input2;
+//    return divResult;
+// }
+
+// //Concatenating Strings with Plus Operator
+// function stringCon(string1, string2) {
+//    var bothstrings = string1 + string2;
+//    return bothstrings;
+// }
+
+// //Concatenating Strings with the Plus Equals Operator
+// function stringConeq(string1, string2) {
+//    string1 += string2;
+//    return string1;
+// }
+
+// //Constructing Strings with Variables
+// function mySentence(name) {
+//    var sentence = "Hi I'm " + name;
+//    return sentence;
+// }
+
+// //Find the Length of a String
+// function findLength(string) {
+//    var lengthIs = string.length;
+//    return lengthIs;
+// }
+
+// //Use Bracket Notation to Find the First Character in a String
+// function firstGuy(string) {
+//    var firstLetter = string[0];
+//    return firstLetter;
+// }
+
+// //Use Bracket Notation to Find the Nth Character in a String
+// function nthGuy(string, n) {
+//    var anyThingGoes = string[n];
+//    return anyThingGoes;
+// }
+
+// //Use Bracket Notation to Find the Last Character in a String
+// function lastGuy(string) {
+//    var onlyTheLast = string[string.length];
+//    return onlyTheLast;
+// }
+
+// //Use Bracket Notation to Find the Nth-to-Last Character in a String
+// function nthToLast(string, n) {
+//    var whateverWorks = string[string.length - n];
+//    return whateverWorks;
+// }
+
+// //Manipulate Arrays With push()
+// function pushIt(array, string) {
+//    var newArray = array.push(string);
+//    return newArray;
+// }
+
+// //Manipulate Arrays With pop()
+// function popIt(array) {
+//    var newwerArray = array.pop();
+//    return newwerArray;
+// }
+
+// //Manipulate Arrays With shift()
+// function shiftIt(array, string) {
+//    var shiftArray = array.shift(string);
+//    return shiftArray;
+// }
+
+// //Manipulate Arrays With unshift()
+// function unShiftIt(array) {
+//    var unShiftArray = array.unshift();
+//    return unShiftArray;
+// }
+
+// //Shopping List
+// function shopping(input1, input2, input3)
+//    var theList
+
+// //Stand in Line
